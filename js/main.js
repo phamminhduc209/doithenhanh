88903b3d9f24f7819faa1498f0df4827247b14bf
phamminhduc209 <minhducpham.it@gmail.com>
2019-03-21 06:42:56 +0700

    Duc

---
+++ js/main.js
@@ -0,0 +1,39 @@
+(function ($) {
+    "use strict";
+    /*
+     * 1. Slide Carousel
+     */
+    $(document).ready(function () {
+        $('.owl-carousel').each(function (index, el) {
+            var config = $(this).data();
+            config.navText = ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'];
+            config.smartSpeed = "800";
+
+            if ($(this).hasClass('owl-style2')) {
+                config.animateOut = 'fadeOut';
+                config.animateIn = 'fadeIn';
+            }
+            if ($(this).hasClass('dotsData')) {
+                config.dotsData = "true";
+            }
+            $(this).owlCarousel(config);
+        });
+    });
+
+    /*
+     * 2. Scroll to Top
+     */
+    $(window).scroll(function () {
+        if ($(this).scrollTop() >= 200) {
+            $('#return-to-top').addClass('td-scroll-up-visible');
+        } else {
+            $('#return-to-top').removeClass('td-scroll-up-visible');
+        }
+    });
+    $('#return-to-top').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 'slow');
+    });
+
+})(jQuery); // End of use strict
